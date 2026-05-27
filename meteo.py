e73e5cc636fb395cb8bbd03b5668874dcafdaf9f
mirek235 <mirek235@gmail.com>
2026-05-27 21:25:31 +0200

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -2,11 +2,13 @@ import os
 import requests
 import tinytuya
 
-# Stažení zabezpečených klíčů z GitHub Secrets
+# Stažení zabezpečených klíčů a domén z GitHub Secrets
 API_KEY = os.environ.get("TUYA_API_KEY")
 API_SECRET = os.environ.get("TUYA_API_SECRET")
 DEVICE_ID = os.environ.get("TUYA_DEVICE_ID")
 ZIVY_OBRAZ_KEY = os.environ.get("ZIVY_OBRAZ_KEY")
+TMEP_DOMAIN_IN = os.environ.get("TMEP_DOMAIN_IN")
+TMEP_DOMAIN_OUT = os.environ.get("TMEP_DOMAIN_OUT")
 
 TUYA_PATH = f"/v2.0/cloud/thing/{DEVICE_ID}/shadow/properties"
 
@@ -53,27 +55,28 @@ try:
         tlak = ziskej_tlak()
 
         # A) ODESLÁNÍ - ŽIVÝ OBRAZ
-        zivy_obraz_url = (
-            f"https://in.zivyobraz.eu/?import_key={ZIVY_OBRAZ_KEY}"
-            f"&teplota_kuchyne={t_kuchyn}&vlhkost_kuchyne={h_kuchyn}"
-            f"&teplota_venku={t_venku}&vlhkost_venku={h_venku}"
-            f"&teplota_loznice={t_loznice}&vlhkost_loznice={h_loznice}"
-            f"&teplota_koupelny={t_koupelna}&vlhkost_koupelny={h_koupelna}"
-        )
-        if tlak:
-            zivy_obraz_url += f"&tlak={tlak}"
-        requests.get(zivy_obraz_url)
+        if ZIVY_OBRAZ_KEY:
+            zivy_obraz_url = (
+                f"https://in.zivyobraz.eu/?import_key={ZIVY_OBRAZ_KEY}"
+                f"&teplota_kuchyne={t_kuchyn}&vlhkost_kuchyne={h_kuchyn}"
+                f"&teplota_venku={t_venku}&vlhkost_venku={h_venku}"
+                f"&teplota_loznice={t_loznice}&vlhkost_loznice={h_loznice}"
+                f"&teplota_koupelny={t_koupelna}&vlhkost_koupelny={h_koupelna}"
+            )
+            if tlak:
+                zivy_obraz_url += f"&tlak={tlak}"
+            requests.get(zivy_obraz_url)
 
         # B) ODESLÁNÍ - TMEP KUCHYŇ
-        if t_kuchyn is not None and h_kuchyn is not None:
-            tmep_in_url = f"http://pechmanovych-in.tmep.cz/?temp={t_kuchyn}&humV={h_kuchyn}"
+        if t_kuchyn is not None and h_kuchyn is not None and TMEP_DOMAIN_IN:
+            tmep_in_url = f"http://{TMEP_DOMAIN_IN}.tmep.cz/?temp={t_kuchyn}&humV={h_kuchyn}"
             if tlak:
                 tmep_in_url += f"&pressV={tlak}"
             requests.get(tmep_in_url)
 
         # C) ODESLÁNÍ - TMEP VENKU
-        if t_venku is not None and h_venku is not None:
-            tmep_out_url = f"http://pechmanovych-out.tmep.cz/?temp={t_venku}&humV={h_venku}"
+        if t_venku is not None and h_venku is not None and TMEP_DOMAIN_OUT:
+            tmep_out_url = f"http://{TMEP_DOMAIN_OUT}.tmep.cz/?temp={t_venku}&humV={h_venku}"
             requests.get(tmep_out_url)
 
         print("Data úspěšně odeslána na Živý obraz i TMEP. Tlak nalezen:", tlak)
